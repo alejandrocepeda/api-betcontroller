30582cab87df293cf635ff231735261ee1de4556
Alejandro Cepeda <alejandrocepeda25@gmail.com>
2018-11-28 01:29:25 -0500

    m

--- routes/api.php
+++ routes/api.php
@@ -25,13 +25,14 @@ Route::get('login', function () {
 Route::post('login', 'Api\Passport\PassportController@login')->name('login');
 Route::post('register', 'Api\User\UserController@register')->name('register');
 
-/*
+
 Route::get('/guzzle', 'Api\Sport\SportController@getGuzzleRequest');
 
 Route::group(['middleware' => 'auth:api'], function(){
+
 	Route::resource('events','Api\Event\EventController');
-	Route::resource('/users', 'Api\User\UserController');	
-	Route::resource('/sports', 'Api\Sport\SportController');
-	Route::resource('/countries', 'Api\Country\CountryController');
+	Route::resource('users', 'Api\User\UserController');	
+	Route::resource('sports', 'Api\Sport\SportController');
+	Route::resource('countries', 'Api\Country\CountryController');
 });
-*/
+
